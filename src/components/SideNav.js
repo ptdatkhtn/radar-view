7c3377237695cbd839e897666360513d8219315b
Duc Phan <phan.duc@futuresplatform.com>
2021-05-24 19:35:44 +0300

    fix: added one more condition for  checking right radar to get right shared-url 3( debug)

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -241,6 +241,8 @@ class SideNav extends PureComponent {
         this.state.publicSharedUserInfo && this.state.publicSharedUserInfo.length > 0 && await drupalApi.fetchShares(groupId).then((data) => {
            data && Object.keys(data).map((i) => {
                 this.state.publicSharedUserInfo.map ((userInfo) => {
+                    console.log('testing = .....', String(node) === String(data[i]['radar_id']))
+                    console.log('data[i]', data[i])
                     if (String(data[i]['user_id']) === String(userInfo?.id) && String(node) === String(data[i]['radar_id'])) {
                         this.setState({
                             publicSharedLink: data[i]['radar_share_url'],
