74c41e690db4039863f664612b78cd813b2207d9
Duc Phan <phan.duc@futuresplatform.com>
2021-07-09 21:59:33 +0300

    Bump @sangre-fp/content-editor from 2.1.16 to 2.1.17

---
+++ build/precache-manifest.ed808914e80cb6a5963a6bdbc0620086.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f2f1356c4673ee53501f1165b09696a",
+    "url": "/radar-app/index.html"
+  },
+  {
+    "revision": "e02bf1f162a233415eda",
+    "url": "/radar-app/static/css/2.045a1c74.chunk.css"
+  },
+  {
+    "revision": "eabce504882745354a5e",
+    "url": "/radar-app/static/css/main.cae46b96.chunk.css"
+  },
+  {
+    "revision": "e02bf1f162a233415eda",
+    "url": "/radar-app/static/js/2.dd3a6228.chunk.js"
+  },
+  {
+    "revision": "31698a2087d3ea5a9e1659da980cab89",
+    "url": "/radar-app/static/js/2.dd3a6228.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eabce504882745354a5e",
+    "url": "/radar-app/static/js/main.9b3d272c.chunk.js"
+  },
+  {
+    "revision": "09579e3e18c75412b291",
+    "url": "/radar-app/static/js/runtime-main.a6f2248f.js"
+  }
+]);
