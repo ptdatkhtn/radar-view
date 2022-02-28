7eaf47b702173192f477f62c9887cf298a6b07e1
Phan <phan.duc@futuresplatform.com>
2022-02-28 02:39:56 +0200

    Fixed: init loading in Add-Content section in Radar view, when using filtering feature, All option should be a default of Group-filter

--- src/containers/SandboxPhenomenaSelectorContainer.js
+++ src/containers/SandboxPhenomenaSelectorContainer.js
@@ -1,5 +1,5 @@
 import { connect } from 'react-redux'
-import { PhenomenaSelector } from '@sangre-fp/content-editor'
+import  PhenomenaSelector  from './PhenomenaSelector'
 
 export default connect(
     state => {
@@ -10,10 +10,21 @@ export default connect(
             }
         } = state
         const isRadar = true
+        const groupsProp = [
+            {
+              value: 0,
+              label: 'FP phenomena'
+            },
+            {
+              value: group?.id,
+              label: group?.title
+            }
+          ]
         return {
             phenomena,
             group,
-            isRadar
+            isRadar,
+            groupsProp
         }
     }
 )(PhenomenaSelector)
